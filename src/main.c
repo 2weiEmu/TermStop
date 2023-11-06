f153ef3df3f67645b7d208f5fa66ef9da71a5ca3
2weiEmu <saalbach.robert@outlook.de>
2023-11-06 22:01:40 +0100

    updated: added flags for more fine grained control of the program

--- src/main.c
+++ src/main.c
@@ -5,12 +5,10 @@
 *  -a (so you can name splits)
 *  -t [NUMBER] so you can set a custom timer sleep
 *  -f [FILE] so you can set a custom output file
-*  -s to silence output of the program except for the timer
+*  -s to silence output of the program except for the timer -> this overrides -a
 *  -d [DELIM] string delimiter selection
-*  -q toggling quotes for the csv file
+*  -q toggling quotes for the csv file to off
 */
-
-#define TIMER_SLEEP_US 45000
 
 // LIBS
 #include <stdio.h>
@@ -20,6 +18,9 @@
 #include <pthread.h>
 #include <string.h>
 #include <fcntl.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 // VARS
 enum COMMAND {
@@ -28,6 +29,15 @@ enum COMMAND {
     QUIT = 'q',
     HALT = 'h'
 };
+
+// default flag values
+int NAMING_FLAG = 0;  // -a flag
+int TIMER_SLEEP_US = 45000; // -t flag
+int USE_FILE_FLAG = 0; // -f flag
+char FILE_OUTPUT_PATH[256] = ""; // -f flag
+char DELIMITER = ','; // -d flag
+int SILENCE_FLAG = 0; // -s flag
+int CSV_QUOTE_FLAG = 1; // -q flag
 
 
 
@@ -67,31 +77,44 @@ void* collect_user_input(void* ) {
 		saved_time = format_time;
 		split_count++;
 
-		// temporarily reset terminal parameters, to the split may be named
-		tcsetattr(STDIN_FILENO, TCSANOW, &oldt); // restore terminal settings
-		pthread_mutex_lock(&mutex);
-		
-		// TODO: should quote characters be allowed? they work, but is it a pain for the person using the csv?
-		printf("Name the new split which occurred at the following time:\n\n");
-		fgets(user_split_name, 199, stdin);
-		// remove the \n from the end, because it also scans that
-		strtok(user_split_name, "\n"); // slightly weird / unusual / unsafe seeming way but it works
+		if (NAMING_FLAG) {
+		    // temporarily reset terminal parameters, to the split may be named
+		    tcsetattr(STDIN_FILENO, TCSANOW, &oldt); // restore terminal settings
+		    pthread_mutex_lock(&mutex);
+		    
+		    // TODO: should quote characters be allowed? they work, but is it a pain for the person using the csv?
+		    printf("Name the new split which occurred at the following time:\n\n");
+		    fgets(user_split_name, 199, stdin);
+		    // remove the \n from the end, because it also scans that
+		    strtok(user_split_name, "\n"); // slightly weird / unusual / unsafe seeming way but it works
 
-		// scanf would be used for formatting a string (scan formatted), use gets to get raw input, it goes until EOF or newline
-		// one of the answers here makes sense https://stackoverflow.com/questions/1247989/how-do-you-allow-spaces-to-be-entered-using-scanf
-		// use fgets to get the input and sscanf to _evaluate_ it. (like the post above says)
-		// sscanf(user_split_name, "%s", user_split_name);
-		
-		// set the terminal back to the required settings and unlock the thread
+		    // scanf would be used for formatting a string (scan formatted), use gets to get raw input, it goes until EOF or newline
+		    // one of the answers here makes sense https://stackoverflow.com/questions/1247989/how-do-you-allow-spaces-to-be-entered-using-scanf
+		    // use fgets to get the input and sscanf to _evaluate_ it. (like the post above says)
+		    // sscanf(user_split_name, "%s", user_split_name);
+		    
+		    // set the terminal back to the required settings and unlock the thread
 
-		// writing the splits to the file
-		fprintf(splits_csv, "\"%d\", \"%s\", \"%s\"\n", split_count, saved_time, user_split_name);
+		    // writing the splits to the file
+		    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+		    pthread_mutex_unlock(&mutex);
+		} else {
+		    sprintf(user_split_name, "Split! Number: %d", split_count);
+		}
 
-		tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-		pthread_mutex_unlock(&mutex);
+		if (USE_FILE_FLAG) {
+		    pthread_mutex_lock(&mutex);
+		    if (CSV_QUOTE_FLAG) {
+			fprintf(splits_csv, "\"%d\"%c \"%s\"%c \"%s\"\n", split_count, DELIMITER, saved_time, DELIMITER, user_split_name);
+		    } else {
+			fprintf(splits_csv, "%d%c %s%c %s\n", split_count, DELIMITER, saved_time, DELIMITER, user_split_name);
+		    }
+		    pthread_mutex_unlock(&mutex);
+		}
 
-
-		printf("\033[AThis is a split. Split info: %s | name: %s %d\n\n", saved_time, user_split_name, split_count);
+		if (!SILENCE_FLAG) {
+		    printf("\033[AThis is a split. Split info: %s | name: %s %d\n\n", saved_time, user_split_name, split_count);
+		}
 		change_user_command(c);
 
 		break;
@@ -120,18 +143,77 @@ void format_timestamp(char format_time[28], suseconds_t diff_us) {
     
 }
 
+
+// default flag values
+// int NAMING_FLAG = 0;  // -a flag
+// int TIMER_SLEEP_US = 45000; // -t flag
+// int USE_FILE_FLAG = 0; // -f flag
+// char FILE_OUTPUT_PATH[256] = ""; // -f flag
+// char DELIMITER = ','; // -d flag
+// int SILENCE_FLAG = 0; // -s flag
+// int CSV_QUOTE_FLAG = 1; // -q flag
+
 // MAIN
 int main(int argc, char** argv) {
 
     // parsing the flags
+    opterr = 0;
+    int c;
 
+    while ((c = getopt(argc, argv, "at:f:d:sq")) != -1) {
+
+	switch (c) {
+
+	    case 'a':
+		NAMING_FLAG = 1;
+		break;
+	    case 't':
+		// TODO: add error check
+		TIMER_SLEEP_US = atoi(optarg);
+		break;
+	    case 'f':
+		USE_FILE_FLAG = 1;
+		strncpy(FILE_OUTPUT_PATH, optarg, 255);
+		break;
+	    case 'd':
+		DELIMITER = optarg[0];
+		break;
+	    case 's':
+		SILENCE_FLAG = 1;
+		break;
+	    case 'q':
+		CSV_QUOTE_FLAG = 0;
+		break;
+		
+	    case '?': // TODO: better errors needed
+		if (optopt == 't')
+		    printf("Unknown t option");
+		else if (optopt == 'f')
+		    printf("No file option given");
+		else if (optopt == 'd')
+		    printf("No new delimiter given");
+		else 
+		    printf("Unknown option.");
+		return 1;
+	    default:
+		abort();
+
+	}
+
+    }
+
+    printf("NAMING_FLAG: %d\nTIMER_SLEEP_US: %d\nUSE_FILE_FLAG: %d\nFILE_OUTPUT_PATH: %s\nDELIMETER: %c\nSILENCE_FLAG: %d\nCSV_QUOTE_FLAG: %d\n",
+	   NAMING_FLAG, TIMER_SLEEP_US, USE_FILE_FLAG, FILE_OUTPUT_PATH, DELIMITER, SILENCE_FLAG, CSV_QUOTE_FLAG);
+
+    // setting up the terminal
     tcgetattr(STDIN_FILENO, &oldt); // Get current terminal settings
     newt = oldt;
     // removes ICANON so that terminal input is not buffered
     newt.c_lflag &= ~(ICANON | ECHO);  // makes sure also that ECHO does not imemdiately give me the char twice
     tcsetattr(STDIN_FILENO, TCSANOW, &newt); // set new attributes
     
-    splits_csv = fopen(default_filepath, "w"); 
+    if (USE_FILE_FLAG) 
+	splits_csv = fopen(FILE_OUTPUT_PATH, "w"); 
 
     // printing the controls
     // TODO: sorry yea, for now hardcoding the printing of <ENTER> -> fix that
@@ -165,7 +247,8 @@ int main(int argc, char** argv) {
 
     pthread_join(thread_id, NULL); // waiting for the input thread to finish
     
-    fclose(splits_csv);
+    if (USE_FILE_FLAG)
+	fclose(splits_csv);
 
     printf("\n"); // purely for formatting
     tcsetattr(STDIN_FILENO, TCSANOW, &oldt); // restore terminal settings
